b4cdcd6ca6b59ac34a404cae85701915f985ff8d
DeltaXrayDelta <53411520+DeltaXrayDelta@users.noreply.github.com>
2021-03-24 15:57:01 +0800

    adguard

---
+++ Ruleset/surge_adguard_convert.py
@@ -0,0 +1,23 @@
+import os
+current_dir_path = os.path.abspath(os.path.dirname(__file__))
+
+adguard_output = []
+target_filename = 'Reject_Carrier_ID.list'
+
+rule_set_raw = open(os.path.join(current_dir_path, target_filename)).readlines()
+for line in rule_set_raw:
+    line = line.strip()
+    # Comments
+    if (line.startswith("#")):
+            adguard_output.append(line.strip())
+    # Generate DOMAIN-SET, combine '.' and URL
+    elif (line.startswith('DOMAIN,')) or (line.startswith('DOMAIN-SUFFIX,')):
+            adguard_output.append(('\n||' + line.split(',',1)[1]).strip() + '^')
+    # New Line
+    elif (line in ['\n', '\r\n']):
+        adguard_output.append(('\r\n'))
+
+
+with open(os.path.join(current_dir_path, str('adguard_' + target_filename)), 'w') as file:
+    for line in adguard_output:
+        file.write('%s\n' % line)
